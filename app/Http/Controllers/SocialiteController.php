580d5b14723193399331d5ea358998a9f4851321
arryaanjain <jainarryaan@gmail.com>
2025-05-03 20:58:14 +0530

    Proper login and save data in db as well :)

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\InvalidStateException;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class SocialiteController extends Controller
 {
@@ -24,11 +26,32 @@ class SocialiteController extends Controller
     {
         try {
             $googleUser = Socialite::driver('google')->user();
-            dd($googleUser);
+            //$user = User::where('google_id', $googleUser->id); doesnt work because we need actual user model, we get that using .first()
+            $user = User::where('google_id', $googleUser->id)->first();
+            if ($user) {
+                Auth::login($user);
+                return redirect()->route('dashboard');
+            } else {
+                // User does not exist, create a new user
+                $userData = User::create([
+                    'name' => $googleUser->name,
+                    'email' => $googleUser->email,
+                    'google_id' => $googleUser->id,
+                    'password' => Hash::make('Password@1234'), // Generate a random password
+                ]);
+
+                if ($userData) {
+                    Auth::login($userData);
+                    return redirect()->route('dashboard');
+                } else {
+                    return redirect()->route('auth.google')->with('error', 'Failed to create user.');
+                }
+            }
+            //dd($googleUser);
         } catch (InvalidStateException $e) {
             dd('Invalid state: ' . $e->getMessage());
-        } catch (\Exception $e) {
-            dd('OAuth error: ' . $e->getMessage());
+        } catch (Exception $e) {
+            dd($e);
         }
     }
 }
